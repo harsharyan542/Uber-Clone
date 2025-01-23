fb913d370a204fd9155eb094f9c93848193eb40f
harsharyan542 <harsharyan542@gmail.com>
2025-01-23 10:35:11 +0530

    profile and login logic

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,9 +2,7 @@ const express = require ('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const userController = require('../controllers/user.controllers' );
-
-
-
+const authMiddleware = require('../middlewares/auth.middleware')
 
 
 router.post('/register',[
@@ -13,6 +11,12 @@ router.post('/register',[
     body('password').isLength({min:6}).withMessage('Password must be 6 character')
 ],userController.registerUser);
 
+router.post('/login',[
+    body('email').isEmail().withMessage('Invaild Email'),
+    body('password').isLength({min:6}).withMessage('Password must be 6 character')
+],userController.loginUser);
+
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile);
 
 
 
