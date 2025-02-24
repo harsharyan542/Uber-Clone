6788f14709d2a29f24f5027f191831eca09cafc0
harsharyan542 <harsharyan542@gmail.com>
2025-02-25 01:52:13 +0530

    ui almost done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,8 @@ import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectedWrapper'
 import CaptainLogout from './pages/CaptainLogout'
+import Riding from './pages/Riding'
+import CaptainRiding from './pages/CaptainRiding'
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
         <Route path ="/UserSignup" element={<UserSignup/>} />
         <Route path ="/CaptainSignup" element={<CaptainSignup/>} />
         <Route path ="/CaptainLogin" element={<CaptainLogin/>} />
+        <Route path ="/riding" element = {<Riding />} />  
+        <Route path ="/captain-riding" element = {<CaptainRiding/>} />
         <Route path='/Home' element={
           <UserProtectedWrapper> 
               <Home/>
